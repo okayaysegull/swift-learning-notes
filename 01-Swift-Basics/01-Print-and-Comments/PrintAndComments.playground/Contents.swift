b8898a1f6b3a0c5ea150d0f2cd11f08bfb13f1ef
Ayşegül Okay <276623712+okayaysegull@users.noreply.github.com>
2026-08-05 17:16:47 +0300

    feat: add print and comments practice

---
+++ 01-Swift-Basics/01-Print-and-Comments/PrintAndComments.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+/*
+ Bu bölüm Swift öğrenme sürecimin
+ ilk kod örneklerini içeriyor.
+*/
+
+print("Ayşegül Okay")
+print("okayaysegull")
+print("iOS geliştiricisi olmak istiyorum.")
+print(2026)
+
+print(
+    "Ayşegül Okay",
+    "iOS geliştiricisi olmak istiyor.",
+    "Başlangıç yılı:",
+    2026
+)
+
+// print(), ekrana bilgi yazdırmamızı sağlayan bir Swift fonksiyonudur.
+// Fonksiyona vereceğimiz bilgiler parantezlerin içine yazılır.
+// Tırnak işaretleri arasında yazılan değer bir metindir.
+// Swift'te metin türüne String denir.
